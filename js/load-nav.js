7c38506ab8726abefc2e2977cffa095b50d68181
Jason Warren <jason.warren@vuluru.com>
2024-12-03 22:00:27 -0500

    HTML POS PRoject productivity app

---
+++ js/load-nav.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("../html/nav.html")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Failed to load navigation.");
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById("nav-placeholder").innerHTML = data;
+    })
+    .catch(error => console.error(error));
+});
